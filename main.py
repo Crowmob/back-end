a1e8f52169b2bf338bdd6dcef98e33dd3a79194c
valera <valerazakolotiuk@gmail.com>
2025-07-08 12:10:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    version="1.0",
+    description="Internship project"
+)
+
+@app.get("/")
+def main():
+    return {"message": "Hello"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
